493c764fc30f706865d3aa89ee4646dcaaa36eb7
Thitinun Chaychayanon(Satang)/ฉายชญาณณณ์ ฐิฏินันน์ <dsst20014@daicelgroup.com>
2020-10-14 14:44:07 +0700

    update

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
-// process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-// process.env.NODE_PORT = process.env.NODE_PORT || '4646';
-// const server = require('./config/express')();
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_PORT = process.env.NODE_PORT || '4646';
+const server = require('./config/express')();
 
-// server.listen(process.env.NODE_PORT, 'localhost', (req, res) => {
-//     console.log('Node server running port : ' + process.env.NODE_PORT);
-// });
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-app.get('/', (req,res) => {
-    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
-})
-const port = process.env.PORT || '5000';
-app.listen(port, () => console.log(`Server started on Port ${port}`));
+server.listen(process.env.NODE_PORT, 'localhost', (req, res) => {
+    console.log('Node server running port : ' + process.env.NODE_PORT);
+});
