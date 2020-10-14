059748bc6ef40d4c0d865e010da7f2695df7a31a
Thitinun Chaychayanon(Satang)/ฉายชญาณณณ์ ฐิฏินันน์ <dsst20014@daicelgroup.com>
2020-10-14 14:40:51 +0700

    update

--- server.js
+++ server.js
@@ -7,5 +7,10 @@
 // });
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+app.get('/', (req,res) => {
+    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
+})
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
