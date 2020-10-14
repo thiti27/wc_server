39e01bfc48a9f300f271228aa451a3e42850538b
Thitinun Chaychayanon(Satang)/ฉายชญาณณณ์ ฐิฏินันน์ <dsst20014@daicelgroup.com>
2020-10-14 14:40:07 +0700

    update

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-process.env.NODE_PORT = process.env.NODE_PORT || '8080';
-const server = require('./config/express')();
+// process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+// process.env.NODE_PORT = process.env.NODE_PORT || '4646';
+// const server = require('./config/express')();
 
 // server.listen(process.env.NODE_PORT, 'localhost', (req, res) => {
 //     console.log('Node server running port : ' + process.env.NODE_PORT);
-
 // });
-
-server.listen(process.env.NODE_PORT , () => {
-    console.log('Node server running port : ' + process.env.NODE_PORT);
-})
-
+const express = require('express');
+const app = express();
+const port = process.env.PORT || '5000';
+app.listen(port, () => console.log(`Server started on Port ${port}`));
